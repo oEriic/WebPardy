3ec64e261298708abaa04f13161e1647c4ca38e9
kyoung224 <60451182+kyoung224@users.noreply.github.com>
2021-03-19 23:14:23 -0400

    Add files via upload

---
+++ log-in_submit.php
@@ -0,0 +1,62 @@
+<html>
+
+  <head>
+    <title> Jeopardy </title>
+    <link rel = "stylesheet" href = "css.css">
+  </head>
+
+  <body>
+    <?php
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $file1 = file("users.txt");
+    $user_input = '';
+
+    if ($username == ''){
+    ?>
+        <div class = center>
+          <h2> Please type in the username </h2>
+          <a href = "homepage.php"> Go back and try again </a>
+        </div>
+    <?php
+    } else if ($password == ''){
+    ?>
+        <div class = center>
+          <h2> Please type in the password </h2>
+          <a href = "homepage.php"> Go back and try again </a>
+        </div>
+    <?php
+    } else{
+      for ($i = 0; $i < count($file1); $i++){
+        $user_input = strstr($file1[$i], $username);
+        if($user_input !== false) {
+          break;
+        }
+      }
+      if ($user_input == ''){
+        ?>
+          <div class = center>
+            <h2> Sorry, that username does not exist. Please try again </h2>
+            <a href = "homepage.php"> Go back and try again </a>
+          </div>
+        <?php
+      } else{
+        $user_inputArr = explode(",", $user_input);
+        $input_password = $user_inputArr[1];
+
+        if ($password != $input_password){
+          ?>
+            <div class = center>
+              <h2> Sorry, that password does not match with the username. Please try again </h2>
+              <a href = "homepage.php"> Go back and try again </a>
+            </div>
+          <?php
+        } else{
+          header("Location: gamepage.php");
+        }
+      }
+    }
+    ?>
+  </body>
+
+</html>
